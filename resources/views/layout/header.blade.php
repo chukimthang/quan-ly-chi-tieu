f7d320f7ae2315a76a3397da973db61de0f8ff3c
chukimthang94 <chukimthang94@gmail.com>
2017-04-19 19:14:52 +0700

    Authentication

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -9,9 +9,30 @@
         </button>
         <a class="navbar-brand" href="#">Home</a>
     </div>
+    
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Logout</a></li>
+             <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" 
+                    role="button" aria-expanded="false">
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}"
+                            method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', ['as' => 'login', 'uses' => 'HomeController@index']);
 
-Route::get('/', function () {
-    return view('index');
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::group(['prefix' => 'category', 'as' => 'category.'], function() {
+        Route::get('/', [
+            'as' => 'index',
+            'uses' => 'CategoryController@index'
+        ]);
+    });
 });
 
-Route::group(['prefix' => 'category', 'as' => 'category.'], function() {
-    Route::get('/', [
-        'as' => 'index',
-        'uses' => 'CategoryController@index'
-    ]);
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
